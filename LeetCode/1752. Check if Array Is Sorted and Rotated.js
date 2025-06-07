b38ff563d692b833737df92c19b3f78cbed7a830
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-06-07 16:59:45 +0530

    Create 1752. Check if Array Is Sorted and Rotated.js

---
+++ LeetCode/1752. Check if Array Is Sorted and Rotated.js
@@ -0,0 +1,17 @@
+// approach: brute force
+// time complexity: O(n)
+// space complexity: O(1)
+
+var check = function (nums) {
+    let count = 0;
+    let n = nums.length;
+    for (let i = 0; i < n; i++) {
+        if (nums[i] > nums[(i + 1) % n]) {
+            count++;
+        }
+        if (count > 1) {
+            return false;
+        }
+    }
+    return true;
+};
