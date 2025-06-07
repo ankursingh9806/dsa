d393b58483dfe133491956f0156529eacca05a60
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-06-07 16:57:41 +0530

    Create 9. Palindrome Number.js

---
+++ LeetCode/9. Palindrome Number.js
@@ -0,0 +1,17 @@
+// approach: math
+// time complexity: O(log n)
+// space complexity: O(1)
+
+var isPalindrome = function (x) {
+    if (x < 0) {
+        return false;
+    }
+    let n = x;
+    let rev = 0;
+    while (n > 0) {
+        let digit = n % 10;
+        rev = rev * 10 + digit;
+        n = Math.floor(n / 10);
+    }
+    return rev === x;
+};
