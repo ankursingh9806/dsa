a296d77f4a81d81280715d6161091be6a2f822a3
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-06-09 20:04:54 +0530

    Create 26. Remove Duplicates from Sorted Array.js

---
+++ LeetCode/26. Remove Duplicates from Sorted Array.js
@@ -0,0 +1,14 @@
+// approach: two pointers
+// time complexity: O(n)
+// space complexity: O(1)
+
+var removeDuplicates = function (nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
