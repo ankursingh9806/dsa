f23bff28837ec5aeb193469bb77d1657287aa269
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-06-10 14:18:17 +0530

    Create 58. Length of Last Word.js

---
+++ LeetCode/58. Length of Last Word.js
@@ -0,0 +1,16 @@
+// approach: brute force
+// time complexity: O(n)
+// space complexity: O(1)
+
+var lengthOfLastWord = function (s) {
+    let count = 0;
+    let i = s.length - 1;
+    while (i >= 0 && s[i] === ' ') {
+        i--;
+    }
+    while (i >= 0 && s[i] !== ' ') {
+        count++;
+        i--;
+    }
+    return count;
+};
