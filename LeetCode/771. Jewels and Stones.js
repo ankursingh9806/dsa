17baef256c08a0b1146c63b09f982dbf4c366a94
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-06-10 14:22:29 +0530

    Create 771. Jewels and Stones.js

---
+++ LeetCode/771. Jewels and Stones.js
@@ -0,0 +1,31 @@
+// approach: brute force
+// time complexity: O(n*m)
+// space complexity: O(1)
+
+var numJewelsInStones = function (jewels, stones) {
+    let count = 0;
+    for (let i = 0; i < stones.length; i++) {
+        for (let j = 0; j < jewels.length; j++) {
+            if (stones[i] === jewels[j]) {
+                count++;
+                break;
+            }
+        }
+    }
+    return count;
+};
+
+// approach: set
+// time complexity: O(n+m)
+// space complexity: O(m)
+
+var numJewelsInStones = function (jewels, stones) {
+    let set = new Set(jewels);
+    let count = 0;
+    for (let i = 0; i < stones.length; i++) {
+        if (set.has(stones[i])) {
+            count++;
+        }
+    }
+    return count;
+};
