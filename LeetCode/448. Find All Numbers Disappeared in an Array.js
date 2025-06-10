fceea1bb6f6e70b276931ac947d94c29eda702d7
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-06-10 14:17:45 +0530

    Create 448. Find All Numbers Disappeared in an Array.js

---
+++ LeetCode/448. Find All Numbers Disappeared in an Array.js
@@ -0,0 +1,34 @@
+// approach: set
+// time complexity: O(n)
+// space complexity: O(n)
+
+var findDisappearedNumbers = function (nums) {
+    let set = new Set(nums);
+    let res = [];
+    for (let i = 1; i <= nums.length; i++) {
+        if (!set.has(i)) {
+            res.push(i);
+        }
+    }
+    return res;
+};
+
+// approach: inplace marking
+// time complexity: O(n)
+// space complexity: O(1)
+
+var findDisappearedNumbers = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+        if (nums[index] > 0) {
+            nums[index] = -nums[index];
+        }
+    }
+    const res = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            res.push(i + 1);
+        }
+    }
+    return res;
+};
