7412695ade15672519fea171f6594376a34266f7
Ankur Singh <162461617+ankursingh9806@users.noreply.github.com>
2025-06-10 14:15:17 +0530

    Create 1480. Running Sum of 1d Array.js

---
+++ LeetCode/1480. Running Sum of 1d Array.js
@@ -0,0 +1,24 @@
+// approach: brute force
+// time complexity: O(n)
+// space complexity: O(n)
+
+var runningSum = function (nums) {
+    let runningSum = [];
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        runningSum.push(sum);
+    }
+    return runningSum;
+};
+
+// approach: inplace
+// time complexity: O(n)
+// space complexity: O(1)
+
+var runningSum = function (nums) {
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] += nums[i - 1];
+    }
+    return nums;
+};
